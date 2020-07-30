7b8d6300e834eba82c0ea31c4338e52865d0732c
Ryan Lee <djrlee51@gmail.com>
2020-07-30 14:48:36 -0700

    added a new switch for Privat/public

--- imports/ui/task.js
+++ imports/ui/task.js
@@ -17,7 +17,7 @@ Template.task.events({
   "click .delete"() {
     Meteor.call("tasks.remove", this._id)
   },
-  "click .toggle-private"() {
+  "click .toggle-private-switch"() {
     Meteor.call("tasks.setPrivate", this._id, !this.private)
   },
 })
